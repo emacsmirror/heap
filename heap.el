37bc0e989b1a33aa32ef8f247f5b08506868ec1f
Toby Cubitt <toby-predictive@dr-qubit.org>
2006-07-31 15:55:31 +0000

    Version 0.10 of the predictive completion package.

--- heap.el
+++ heap.el
@@ -1,29 +1,31 @@
 
 ;;; heap.el --- heap (a.k.a. priority queue) data structure package
 
+
 ;; Copyright (C) 2004 2005 Toby Cubitt
 
-;; Author: Toby Cubitt
+;; Author: Toby Cubitt <toby-predictive@dr-qubit.org>
 ;; Version: 0.1.2
 ;; Keywords: heap, priority queue
+;; URL: http://www.dr-qubit.org/emacs.php
+
 
 ;; This file is part of the Emacs Predictive Completion package.
 ;;
-;; The Emacs Predicive Completion package is free software; you can
-;; redistribute it and/or modify it under the terms of the GNU
-;; General Public License as published by the Free Software
-;; Foundation; either version 2 of the License, or (at your option)
-;; any later version.
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
 ;;
-;; The Emacs Predicive Completion package is distributed in the hope
-;; that it will be useful, but WITHOUT ANY WARRANTY; without even the
-;; implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-;; PURPOSE.  See the GNU General Public License for more details.
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with the Emacs Predicive Completion package; if not, write
-;; to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
-;; Boston, MA 02111-1307 USA
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+;; MA 02110-1301, USA.
 
 
 ;;; Commentary:
