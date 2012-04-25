aa998aefd9dd99d65d3a22e7449deb5adc710183
Toby S. Cubitt <toby-predictive@dr-qubit.org>
2012-04-25 14:05:45 +0200

    Updated copyright attribution and license (GPL2 -> GPL3).

--- heap.el
+++ heap.el
@@ -2,7 +2,7 @@
 ;;; heap.el --- heap (a.k.a. priority queue) data structures
 
 
-;; Copyright (C) 2004-2006, 2008, 2012 Toby Cubitt
+;; Copyright (C) 2004-2006, 2008, 2012  Free Software Foundation, Inc
 
 ;; Author: Toby Cubitt <toby-predictive@dr-qubit.org>
 ;; Version: 0.3
@@ -10,18 +10,18 @@
 ;; URL: http://www.dr-qubit.org/emacs.php
 
 
-;; This file is NOT part of Emacs.
+;; This file is part of Emacs.
 ;;
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
